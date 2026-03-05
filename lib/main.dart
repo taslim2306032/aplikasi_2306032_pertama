d20b638750a935ee33f399d8e4b638407efb2ec2
Taslim Nuralim <2306032@itg.ac.id>
2026-03-05 16:56:47 +0700

    upload project flutter modul 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,139 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    String title = 'Aplikasi Pertamaku';
-    String name = 'Taslim Nuralim';
-    String message = 'Halo nama saya $name, selamat datang di $title';
-
     return MaterialApp(
-      title: title,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 9, 125, 44),
-          title: Text(title),
+          backgroundColor: Colors.red[800],
+          leading: Icon(Icons.home),
+          title: Text("Aplikasi Pertamaku"),
         ),
-        body: Center(child: Text(message)),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Icon(Icons.archive),
+                  SizedBox(width: 10),
+                  Text("Artikel Baru"),
+                ],
+              ),
+
+              Card(
+                child: Column(
+                  children: [
+                    Image.network(
+                      "assets/images/IMG-20230801-WA0086.jpg",
+                      width: 120,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(height: 10),
+                    Text("HALLO NAMA SAYA TASLIM GANTENG"),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              Row(
+                children: [
+                  Text(
+                    "Komentar",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+
+              Expanded(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(13),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("John Doe"),
+                            Text("Lorem ipsum dolor sit amet."),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(13),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Mary Doe"),
+                            Text("Lorem ipsum dolor sit amet."),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(13),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Alex Doe"),
+                            Text("Lorem ipsum dolor sit amet."),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(13),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Jane Doe"),
+                            Text("Lorem ipsum dolor sit amet."),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(13),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Michael Doe"),
+                            Text("Lorem ipsum dolor sit amet."),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
